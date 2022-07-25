7f944d0f7ea662058501fa580678fe52494e7739
Kevin Pelaez <kpelaez@omnimedica.com.ar>
2022-07-25 13:34:42 -0300

    Se agrego una variable de fecha para mantener siempre actualizado el dia en la consulta a la API

--- PruebaFormatosyFechas.py
+++ PruebaFormatosyFechas.py
@@ -7,8 +7,10 @@ from datetime import *
 
 Token = GeneradorToken('a843eb5642cbc99fed7af5d62fc1c764','176acd327ffd43fd858680d4a7efc475')
 
-#Traemos la fecha en el formato predeterminado por finnegans. 
-fecha_Hoy = datetime.today().strftime('%Y-%m-%d') ##Formato yyyy-mm-dd
+#Probamos traer la fecha en el formato predeterminado por finnegans. 
+fecha_Hoy = datetime.today().strftime('%Y-%m-%d')
+
+print(fecha_Hoy)
 
 urlComp= f'https://api.teamplace.finneg.com/api/reports/COMPOSICIONSALDOSCLIENTES?ACCESS_TOKEN={Token}&PARAMWEBREPORT_fecha={fecha_Hoy}&PARAMWEBREPORT_organizacion=&PARAMWEBREPORT_circuitocontable=&PARAMWEBREPORT_cuenta=&PARAMWEBREPORT_moneda=&PARAMWEBREPORT_Empresa='
 
@@ -19,12 +21,3 @@ if Consulta.status_code == 200:
     print("Ingreso ok")
 else:
     print("No ingreso!")
-
-
-df = json_normalize(Consulta.json())
-
-#df.to_excel('C:/Users/KPelaez.OMNI/Desktop/ComposicionClientes.xlsx')
-
-#Generamos un csv de esta consulta       
-
-df.to_csv('G:\Mi unidad\Python\ComposicionFavaloroV1.csv')    
